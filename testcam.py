a2acca88027d915ccd98645838cd1d6c398ff886
Shane Guan <shguan10@gmail.com>
2021-10-13 10:20:59 -0700

    save_img

--- testcam.py
+++ testcam.py
@@ -1,11 +1,21 @@
 import cv2 
-
 import numpy as np
+import pdb
+
 MAXCOLS = 1280
 MAXROWS = 720
 RADIUS = 10
+SAVEDIR = "data"
+
 def generate_random_circle():
-  return np.random.rand(2) * [MAXROWS,MAXCOLS]
+  return (np.random.rand(2) * [MAXROWS,MAXCOLS]).astype(int)
+
+def save_frame(frame,circle_position):
+  name = "/".join([SAVEDIR,
+                   str(circle_position[0])+"_"+str(circle_position[1])+".jpg"
+                   ])
+  cv2.imwrite(name,frame)
+  return name
 
 def main():
   cap = cv2.VideoCapture(0)
@@ -19,15 +29,17 @@ def main():
   while True:
     _, frame = cap.read()
 
-    draw_circle(frame,circle_position)
+    keypress = cv2.waitKey(1)
+    # draw_circle(frame,circle_position)
 
     cv2.imshow("Live Feed",frame)
 
-    if cv2.waitKey(1) == ord(" "):
+    if keypress == ord(" "):
       save_frame(frame,circle_position)
+
       circle_position = generate_random_circle()
 
-    if cv2.waitKey(1) == ord("q"):
+    elif keypress == ord("q"):
       break
 
   cap.release()
