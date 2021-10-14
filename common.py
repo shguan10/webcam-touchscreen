6cb8357b4331dcfea41c94dd9b295e94c41cbc69
Shane Guan <shguan10@gmail.com>
2021-10-14 15:06:09 -0700

    first sample code

--- common.py
+++ common.py
@@ -6,6 +6,8 @@ import os, sys
 MAXCOLS = 1280
 MAXROWS = 720
 
+WIN = (325,63)
+
 TRAP_TOP_LEFT = (50,470)
 TRAP_TOP_RIGHT = (50,780)
 TRAP_BOT_LEFT = (MAXROWS,0)
@@ -15,6 +17,24 @@ RADIUS = 20
 
 THRESHOLD = 8000
 BACKGROUND_FNAME = "background.jpg"
+
+def is_right_of(linep1,linep2,p):
+  # use the cross product to determine whether p is to the right of the line from linep1 to linep2 (right thumb out of the screen)
+  # all points are in (row,col) = (y,x) format
+  return ((linep2[1]-linep1[1])*(p[0]-linep1[0])-(linep2[0]-linep1[0])*(p[1]-linep1[1]))>0
+
+def is_valid(p):
+  if p[0]<TRAP_TOP_LEFT[0]: return False
+  if p[0]>TRAP_BOT_LEFT[0]: return False
+  if not is_right_of(TRAP_BOT_LEFT,TRAP_TOP_LEFT,p): return False
+  if is_right_of(TRAP_BOT_RIGHT,TRAP_TOP_RIGHT,p): return False
+  return True
+
+def generate_random_circle():
+  p = (0,0)
+  while not is_valid(p):
+    p = (np.random.rand(2) * [MAXROWS,MAXCOLS]).astype(int)
+  return p
 
 def open_image(fname="data/346_637.jpg"):
   frame = cv2.imread(fname)
@@ -113,7 +133,7 @@ def sample_finger(cap):
     canvas = draw_circle(canvas,circle_position)
 
     cv2.imshow("sampling finger",canvas)
-
+    cv2.moveWindow("sampling finger",WIN[0],WIN[1])
     if keypress == ord(" "):
       circle = read_circle(frame,circle_position)
       cv2.destroyAllWindows()
@@ -128,7 +148,9 @@ def sample_background(cap):
     frame = cv2.flip(frame, 1)
     keypress = cv2.waitKey(1)
 
-    cv2.imshow("sampling background * "+str(times)+" times",frame)
+    name = "sampling background * "+str(times)+" times"
+    cv2.imshow(name,frame)
+    cv2.moveWindow(name,WIN[0],WIN[1])
 
     if keypress == ord(" "):
       times-=1
@@ -141,6 +163,9 @@ def sample_background(cap):
   return arr
 
 def filter(cap,finger_pixels,background):
+  cv2.namedWindow("filtered")
+  cv2.moveWindow("filtered",WIN[0],WIN[1])
+  
   while True:
     _, frame = cap.read()
     keypress = cv2.waitKey(1)
@@ -160,6 +185,33 @@ def filter(cap,finger_pixels,background):
 
     if keypress == ord("q"):
       break
+  cv2.destroyAllWindows()
+
+def mousecb(event,x,y,flags,userdata):
+  if event == cv2.EVENT_LBUTTONDOWN:
+    userdata.x = x
+    userdata.y = y
+
+class Userdata:
+  def __init__(self):
+    self.x = None
+    self.y = None
+    self.frame = None
+
+def get_sample(cap,finger_pixels,background):
+  cv2.namedWindow("filtered")
+  cv2.moveWindow("filtered",WIN[0],WIN[1])
+  ud = Userdata()
+  cv2.setMouseCallback("filtered",mousecb,ud)
+
+  while ud.x is None:
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+    frame = background_filter(frame,background)
+    frame = fuzzy_mask(frame,pixels=finger_pixels)
+
+  cv2.imwrite("data/"+str(ud.x)+"_"+str(ud.y)+".jpg",frame)
   cv2.destroyAllWindows()
   
 
